2641885ae26791dfa91da821342aade01fda22d0
ProgramacaoEco <programacaoecosistemas@gmail.com>
2024-05-25 22:24:40 -0300

    feat: :lipstick: create home page + components

---
+++ src/components/basis/Drawer/DrawerSettings/style.css.ts
@@ -0,0 +1,77 @@
+import { viewPort } from "@/theme/constants";
+import { themeVars } from "@/theme/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const drawerContentContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  position: "relative",
+});
+
+export const drawerSettingsContent = style({
+  width: "300px",
+  backgroundColor: "white",
+  "@media": {
+    [viewPort.small]: {
+      width: "100vw",
+    },
+  },
+});
+
+export const drawerCloseButtonMobile = style({
+  display: "none",
+  "@media": {
+    [viewPort.small]: {
+      display: "flex",
+      justifyContent: "flex-end",
+    },
+  },
+});
+
+export const drawerTile = style({
+  padding: "10px",
+  display: "flex",
+  width: "100%",
+  fontSize: "1.4rem",
+  alignItems: "center",
+  backgroundColor: "#F2F2F2",
+  textDecoration: "none",
+  color: themeVars.color.background,
+  userSelect: "none",
+});
+
+export const drawerTileLabel = style({
+  marginLeft: "40px",
+});
+
+export const drawerHeader = style({
+  position: "relative",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  padding: "30px",
+  fontSize: "1.5rem",
+  textAlign: "center",
+  backgroundColor: themeVars.color.background,
+  color: "white",
+});
+
+export const drawerHeaderCloseButton = style({
+  display: "none",
+  position: "absolute",
+  right: 0,
+  "@media": {
+    [viewPort.small]: {
+      display: "flex",
+    },
+  },
+});
+
+export const drawerFooter = style({
+  position: "absolute",
+  left: "0",
+  right: "0",
+  bottom: "0",
+});
