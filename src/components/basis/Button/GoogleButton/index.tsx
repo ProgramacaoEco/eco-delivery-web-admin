f4e886b8ac95d69cf214dce84c2bab8ec8ec0eb2
Victor Proppe <vctudo.27@gmail.com>
2024-05-30 13:59:45 -0300

    feat: :lipstick: refac components to use Typography

--- src/components/basis/Button/GoogleButton/index.tsx
+++ src/components/basis/Button/GoogleButton/index.tsx
@@ -1,7 +1,9 @@
 import { ComponentProps, PropsWithChildren } from "react";
 
 import GoogleLogo from "@icons/GoogleLogo";
+import { Typography } from "../../Typography";
 import { googleButton } from "./style.css";
+import { themeVars } from "@/theme/theme.css";
 
 export default function GoogleButton({
   children,
@@ -10,7 +12,12 @@ export default function GoogleButton({
   return (
     <button className={googleButton} {...props}>
       <div>
-        <GoogleLogo style={{ verticalAlign: "text-top" }} /> {children}
+        <GoogleLogo
+          style={{ verticalAlign: "text-top", marginRight: "0.8125rem" }}
+        />{" "}
+        <Typography.Caption color={themeVars.color.background}>
+          {children}
+        </Typography.Caption>
       </div>
     </button>
   );
