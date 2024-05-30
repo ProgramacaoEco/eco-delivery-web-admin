89f98b268be0d800ec3d9e2050e3d4ec95a01387
Victor Proppe <vctudo.27@gmail.com>
2024-05-30 13:59:56 -0300

    feat: :lipstick: refac components to use Typography

--- src/components/basis/Drawer/DrawerSettings/drawerTile.tsx
+++ src/components/basis/Drawer/DrawerSettings/drawerTile.tsx
@@ -1,24 +1,26 @@
-import Link from "next/link";
 import { drawerTile, drawerTileLabel } from "./style.css";
-import Image from "next/image";
+
+import { IconType } from "@/utils/IconType";
+import Link from "next/link";
+import { Typography } from "../../Typography";
+import { themeVars } from "@/theme/theme.css";
 
 interface DrawerTileProps {
-  iconUrl: string;
+  Icon: IconType;
   label: string;
   href: string;
 }
 
-export default function DrawerTile({ iconUrl, label, href }: DrawerTileProps) {
+export default function DrawerTile({ Icon, label, href }: DrawerTileProps) {
   return (
     <Link href={href} className={drawerTile}>
-      <Image
-        src={iconUrl}
-        width={48}
-        height={48}
-        alt={iconUrl}
-        draggable={false}
-      />
-      <div className={drawerTileLabel}>{label}</div>
+      <Icon fontSize={36} />
+      <Typography.DisplayLargeRegular
+        color={themeVars.color.background}
+        className={drawerTileLabel}
+      >
+        {label}
+      </Typography.DisplayLargeRegular>
     </Link>
   );
 }
