a2636d9539d793791f4d272d89b0cfb91f6f00ae
Victor Proppe <vctudo.27@gmail.com>
2024-05-30 13:58:21 -0300

    feat: :lipstick: create Typography component & variants

---
+++ src/components/basis/Typography/bold/DisplayMedium.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from "react";
+import { TypographyProps } from "@components/basis/Typography";
+import { cn } from "@/utils/classNames";
+import { displayMedium } from "./style.css";
+import { themeVars } from "@/theme/theme.css";
+
+export default function DisplayMedium({
+  children,
+  color = themeVars.color.common.white,
+  className = undefined,
+}: PropsWithChildren<TypographyProps>) {
+  const displayStyle: React.CSSProperties = {
+    color: color,
+  };
+
+  return (
+    <h3 className={cn(displayMedium, className)} style={displayStyle}>
+      {children}
+    </h3>
+  );
+}
