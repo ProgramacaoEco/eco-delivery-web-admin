f8ee2e5381b300da419a36f5f3e3c3ad252dba5f
Victor Proppe <vctudo.27@gmail.com>
2024-05-30 17:24:33 -0300

    feat: :lipstick: implement login page

---
+++ src/app/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import {
+  container,
+  imageContainer,
+  loginContainer,
+  loginContent,
+  welcomeBackTitle,
+} from "./style.css";
+
+import GoogleButton from "@/components/basis/Button/GoogleButton";
+import Image from "next/image";
+import LinkButton from "@/components/basis/LinkButton/LinkButton";
+import { Typography } from "@/components/basis/Typography";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { viewPort } from "@/theme/constants";
+
+const Login = () => {
+  const matchMedia = useMediaQuery(viewPort.small);
+
+  return (
+    <main className={container}>
+      {matchMedia ? null : (
+        <Image
+          className={imageContainer}
+          alt="Logo ECO"
+          src="/login_hero_image.png"
+          height={0}
+          width={2280}
+          style={{
+            objectFit: "cover",
+            height: "100%",
+            width: "100%",
+          }}
+          draggable={false}
+          priority
+        />
+      )}
+
+      <div className={loginContainer}>
+        <div className={loginContent}>
+          <Image
+            alt="Logo ECO"
+            src="/logo.png"
+            draggable={false}
+            width={128}
+            height={128}
+            priority
+          />
+          <Typography.TitleLight className={welcomeBackTitle}>
+            Bem vindo de volta!
+          </Typography.TitleLight>
+          <Typography.TitleLight>
+            É sempre bom vê-lo novamente.
+          </Typography.TitleLight>
+          <GoogleButton href="/home" onClick={() => {}} />
+          <LinkButton href="#">Precisa de ajuda?</LinkButton>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
