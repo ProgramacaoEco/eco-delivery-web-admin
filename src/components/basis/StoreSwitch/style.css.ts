ca6e3ba76c10d229b5fa3163634998fd3574d03f
Victor Proppe <vctudo.27@gmail.com>
2024-05-30 14:05:14 -0300

    refactor: :lipstick: layout refinement

--- src/components/basis/StoreSwitch/style.css.ts
+++ src/components/basis/StoreSwitch/style.css.ts
@@ -2,4 +2,9 @@ import { style } from "@vanilla-extract/css";
 
 export const storeOpenColor = style({
   color: "#00FF00",
+  fontWeight: "bolder",
+});
+
+export const storeClosed = style({
+  fontWeight: "bolder",
 });
