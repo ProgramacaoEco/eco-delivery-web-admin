c32c72d6f1179f480d6445949e339894359deee2
Victor Proppe <vctudo.27@gmail.com>
2024-05-30 14:03:18 -0300

    feat: :lipstick: create Typography component & variants

---
+++ src/components/basis/Typography/index.tsx
@@ -0,0 +1,20 @@
+import Caption from "./bold/Caption";
+import DisplayLargeRegular from "./regular/DisplayLarge";
+import DisplayMediumBold from "./bold/DisplayMedium";
+import TitleBold from "./bold/Title";
+import TitleLight from "./light/Title";
+import TitleRegular from "./regular/Title";
+
+export interface TypographyProps {
+  color?: string;
+  className?: string;
+}
+
+export const Typography = {
+  Caption,
+  DisplayMediumBold,
+  TitleBold,
+  TitleLight,
+  DisplayLargeRegular,
+  TitleRegular,
+};
