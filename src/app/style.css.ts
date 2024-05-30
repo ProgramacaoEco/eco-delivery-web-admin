13c2452888c3c060965e0fb0607ed09b01f85ca0
Victor Proppe <vctudo.27@gmail.com>
2024-05-30 17:24:38 -0300

    feat: :lipstick: implement login page

---
+++ src/app/style.css.ts
@@ -0,0 +1,48 @@
+import { style } from "@vanilla-extract/css";
+import { viewPort } from "@/theme/constants";
+
+export const container = style({
+  display: "grid",
+  gridTemplateColumns: "0.5fr 0.5fr",
+  gridTemplateRows: "100vh",
+  "@media": {
+    [viewPort.small]: {
+      minHeight: "100svh",
+      gridTemplateColumns: "1fr",
+      gridTemplateRows: "1fr",
+      padding: "30px",
+    },
+  },
+});
+
+export const loginContainer = style({
+  display: "flex",
+  position: "relative",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "10px",
+});
+
+export const loginContent = style({
+  textAlign: "center",
+  position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "20px",
+  bottom: "120px",
+  "@media": {
+    [viewPort.small]: {
+      bottom: "0px",
+    },
+  },
+});
+
+export const welcomeBackTitle = style({
+  marginBottom: "50px",
+});
+
+export const imageContainer = style({
+  height: "100%",
+});
