92d45e345048f0f3c6c15f8fc9596da8dbe6af94
Victor Proppe <vctudo.27@gmail.com>
2024-06-07 15:45:58 -0300

    refactor: :recycle: adjust bugging points in auth & autorization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,19 @@
+import { NextResponse } from "next/server";
 import { auth } from "@/auth";
 
 export default auth((req) => {
   const isAuth = !!req.auth;
   const pathname = req.nextUrl.pathname;
 
-  if (!isAuth && pathname !== "/" && pathname !== "/signin") {
+  if (!isAuth && pathname !== "/" && !pathname.startsWith("/signin")) {
     return Response.redirect(new URL("/", req.url));
   }
 
-  if (isAuth && (pathname === "/" || pathname === "/signin")) {
+  if (isAuth && (pathname === "/" || pathname.startsWith("/signin"))) {
     return Response.redirect(new URL("/home", req.url));
   }
+
+  return NextResponse.next();
 });
 
 export const config = {
