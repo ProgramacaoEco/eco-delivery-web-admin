6410fbbf7abb7d06a0e5f56aa41c26c4903321ee
Victor Proppe <vctudo.27@gmail.com>
2024-06-07 15:45:42 -0300

    refactor: :recycle: adjust bugging points in auth & autorization

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import { continueWithGoogle, signin } from "./style.css";
+
 import GoogleButton from "@/components/basis/Button/GoogleButton";
 import { providerData } from "@/auth";
 import { signIn } from "next-auth/react";
 
 export default function SignInPage() {
   return (
-    <GoogleButton
-      action={async () =>
-        await signIn(providerData.id, { redirect: true, callbackUrl: "/home" })
-      }
-    />
+    <div className={signin}>
+      <div className={continueWithGoogle}>
+        <GoogleButton
+          label="Prosseguir para o Google"
+          action={async () =>
+            await signIn(providerData.id, {
+              redirect: true,
+              callbackUrl: "/home",
+            })
+          }
+        />
+      </div>
+    </div>
   );
 }
