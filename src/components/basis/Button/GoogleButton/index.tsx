0e49a2526cb40943fc398a36e37633c113493266
Victor Proppe <vctudo.27@gmail.com>
2024-06-07 00:45:41 -0300

    feat: :sparkles: route autorization

--- src/components/basis/Button/GoogleButton/index.tsx
+++ src/components/basis/Button/GoogleButton/index.tsx
@@ -1,25 +1,17 @@
 import GoogleLogo from "@icons/GoogleLogo";
-import Link from "next/link";
 import { Typography } from "../../Typography";
 import { googleButton } from "./style.css";
 import { themeVars } from "@/theme/theme.css";
 
-interface GoogleButtonProps {
-  href: string;
-  onClick?: () => void;
-}
-
-export default function GoogleButton({ ...props }: GoogleButtonProps) {
+export default function GoogleButton({ action }: { action: () => void }) {
   return (
-    <Link className={googleButton} {...props}>
-      <div>
-        <GoogleLogo
-          style={{ verticalAlign: "text-top", marginRight: "0.8125rem" }}
-        />{" "}
-        <Typography.Caption color={themeVars.color.background}>
-          Continuar com o Google
-        </Typography.Caption>
-      </div>
-    </Link>
+    <button className={googleButton} onClick={action}>
+      <GoogleLogo
+        style={{ verticalAlign: "text-top", marginRight: "0.8125rem" }}
+      />{" "}
+      <Typography.Caption color={themeVars.color.background}>
+        Continuar com o Google
+      </Typography.Caption>
+    </button>
   );
 }
