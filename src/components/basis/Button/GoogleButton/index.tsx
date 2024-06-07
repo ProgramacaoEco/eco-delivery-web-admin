0cfed91c9fdd0d2136c88998206832f19145f2e4
Victor Proppe <vctudo.27@gmail.com>
2024-06-07 15:45:47 -0300

    refactor: :recycle: adjust bugging points in auth & autorization

--- src/components/basis/Button/GoogleButton/index.tsx
+++ src/components/basis/Button/GoogleButton/index.tsx
@@ -3,14 +3,20 @@ import { Typography } from "../../Typography";
 import { googleButton } from "./style.css";
 import { themeVars } from "@/theme/theme.css";
 
-export default function GoogleButton({ action }: { action: () => void }) {
+export default function GoogleButton({
+  action,
+  label = "Continuar com o Google",
+}: {
+  action: () => void;
+  label?: string;
+}) {
   return (
     <button className={googleButton} onClick={action}>
       <GoogleLogo
         style={{ verticalAlign: "text-top", marginRight: "0.8125rem" }}
       />{" "}
       <Typography.Caption color={themeVars.color.background}>
-        Continuar com o Google
+        {label}
       </Typography.Caption>
     </button>
   );
