3370251b5f03098c441dd645fd804a79a25422b9
Victor Proppe <vctudo.27@gmail.com>
2024-06-07 21:08:58 -0300

    feat: :sparkles: check authorized user on sign in

--- auth.ts
+++ auth.ts
@@ -1,3 +1,5 @@
+import { Collections } from "@/helpers/firestore/collections";
+import { FirestoreHelper } from "@/helpers/firestore";
 import Google from "next-auth/providers/google";
 import NextAuth from "next-auth";
 
@@ -7,6 +9,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [provider],
   pages: {
     signIn: "/signin",
+    error: "/error",
+  },
+  callbacks: {
+    async signIn({ account, profile }) {
+      const user = await FirestoreHelper.getBy(
+        profile?.email ?? "",
+        Collections.Usuarios
+      );
+      const userExists = !!user;
+      return userExists;
+    },
   },
 });
 
