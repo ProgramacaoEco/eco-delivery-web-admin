9d3543a5bf08951515f5e27dfeb717a9d975301a
Victor Proppe <vctudo.27@gmail.com>
2024-06-11 16:01:40 -0300

    eat: :construction: impl user management page

---
+++ src/components/basis/LoadingContainer/style.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const loadingContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  minHeight: "100vh",
+  width: "100%",
+  padding: "1.25rem",
+});
