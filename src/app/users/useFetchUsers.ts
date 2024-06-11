9624238ee41d4664c6541711794699d54f9392b9
Victor Proppe <vctudo.27@gmail.com>
2024-06-11 16:01:26 -0300

    eat: :construction: impl user management page

---
+++ src/app/users/useFetchUsers.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+import { Collections } from "@/helpers/firestore/collections";
+import { FirestoreHelper } from "@/helpers/firestore";
+import { User } from "@/helpers/firestore/model/user";
+
+export default function useFetchUsers() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<User[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    FirestoreHelper.get(Collections.Usuarios)
+      .then((users) => {
+        if (!!users) {
+          setData(
+            users.map((user) => new User(user.id, user.email, user.name))
+          );
+        } else {
+          setData([]);
+          setError(true);
+        }
+      })
+      .catch((error) => {
+        setError(true);
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return { loading, data, error };
+}
