81cd41b334d4a5cfc4724ec7449c9abf2f4558e8
Victor Proppe <vctudo.27@gmail.com>
2024-06-11 16:01:22 -0300

    eat: :construction: impl user management page

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,24 +1,33 @@
 "use client";
 
+import ListTile from "@/components/basis/ListTile";
+import LoadingContainer from "@/components/basis/LoadingContainer";
 import NewUserForm from "./NewUserForm";
 import PageTitle from "@/components/basis/PageTitle/PageTitle";
+import Tile from "@/components/basis/Tile";
 import { themeVars } from "@/theme/theme.css";
+import useFetchUsers from "./useFetchUsers";
+import { users } from "./style.css";
 
 export default function Users() {
+  const { data, error, loading } = useFetchUsers();
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        alignItems: "center",
-      }}
-    >
-      <PageTitle
-        color={themeVars.color.users.appbarColor}
-        title="Configurações > Usuários"
-      />
-      <NewUserForm />
-    </div>
+    <LoadingContainer loading={loading} error={error}>
+      <div className={users}>
+        <PageTitle color={themeVars.color.users.appbarColor} title="Usuários" />
+        <div style={{ marginTop: "20px", width: "inherit" }}>
+          <NewUserForm />
+        </div>
+
+        <ListTile>
+          {data.map(({ userName }, index) => (
+            <Tile key={userName} count={index}>
+              {userName}
+            </Tile>
+          ))}
+        </ListTile>
+      </div>
+    </LoadingContainer>
   );
 }
