fadf3699ca2c4ef3464c3308e9b923baa748663a
Victor Proppe <vctudo.27@gmail.com>
2024-06-11 16:01:48 -0300

    eat: :construction: impl user management page

---
+++ src/components/basis/Tile/style.css.ts
@@ -0,0 +1,16 @@
+import { style } from "@vanilla-extract/css";
+
+export const tileContainer = style({
+  backgroundColor: "#7A7A7A",
+  height: "40px",
+  width: "100%",
+});
+
+export const tile = style({
+  height: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  textTransform: "uppercase",
+  padding: "10px",
+});
