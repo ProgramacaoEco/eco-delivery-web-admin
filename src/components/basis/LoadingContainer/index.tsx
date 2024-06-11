94fb293bcedad042d790ea9cbfa26bb95cce5d2b
Victor Proppe <vctudo.27@gmail.com>
2024-06-11 16:01:37 -0300

    eat: :construction: impl user management page

---
+++ src/components/basis/LoadingContainer/index.tsx
@@ -0,0 +1,36 @@
+import Loading from "../Loading";
+import { PropsWithChildren } from "react";
+import { Typography } from "../Typography";
+import { loadingContainer } from "./style.css";
+
+interface LoadingContainerProps {
+  error: boolean;
+  loading: boolean;
+}
+export default function LoadingContainer({
+  error,
+  loading,
+  children,
+}: PropsWithChildren<LoadingContainerProps>) {
+  if (!loading && error) {
+    return (
+      <div className={loadingContainer}>
+        <Typography.TitleBold>
+          Desculpe-nos, parece que algo de errado aconteceu. Por favor,
+          recarregue a página e tente novamente. Se o erro persistir, contate o
+          suporte.
+        </Typography.TitleBold>
+      </div>
+    );
+  }
+
+  if (loading && !error) {
+    return (
+      <div className={loadingContainer}>
+        <Loading />
+      </div>
+    );
+  }
+
+  return children;
+}
