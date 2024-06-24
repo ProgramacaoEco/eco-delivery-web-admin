f9d7b13d86977aafece68531c89831a92072b055
Victor Proppe <vctudo.27@gmail.com>
2024-06-24 18:02:06 -0300

    feat: :sparkles: implement users management

---
+++ src/app/users/context/type.ts
@@ -0,0 +1,19 @@
+import { Dispatch, SetStateAction } from "react";
+
+import { User } from "@/helpers/firestore/model/user";
+
+export type UserContextType = {
+  data?: User[];
+  successMessage?: string;
+  success?: boolean;
+  errorMessage?: string;
+  error: boolean;
+  loading: boolean;
+  setSuccess: Dispatch<SetStateAction<boolean | undefined>>;
+  setSuccessMessage: Dispatch<SetStateAction<string | undefined>>;
+  setErrorMessage: Dispatch<SetStateAction<string | undefined>>;
+  setData: Dispatch<SetStateAction<User[] | undefined>>;
+  setError: Dispatch<SetStateAction<boolean>>;
+  setLoading: Dispatch<SetStateAction<boolean>>;
+  reset: () => void;
+};
