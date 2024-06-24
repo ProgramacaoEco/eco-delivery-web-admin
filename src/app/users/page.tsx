cc24b63371a2290f60e7a3386b74ea0e455a7d3e
Victor Proppe <vctudo.27@gmail.com>
2024-06-24 18:01:44 -0300

    feat: :sparkles: implement users management

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,33 +1,12 @@
 "use client";
 
-import ListTile from "@/components/basis/ListTile";
-import LoadingContainer from "@/components/basis/LoadingContainer";
-import NewUserForm from "./NewUserForm";
-import PageTitle from "@/components/basis/PageTitle/PageTitle";
-import Tile from "@/components/basis/Tile";
-import { themeVars } from "@/theme/theme.css";
-import useFetchUsers from "./useFetchUsers";
-import { users } from "./style.css";
+import UserProvider from "./context/UserProvider";
+import UsersComponent from "./UserComponent";
 
 export default function Users() {
-  const { data, error, loading } = useFetchUsers();
-
   return (
-    <LoadingContainer loading={loading} error={error}>
-      <div className={users}>
-        <PageTitle color={themeVars.color.users.appbarColor} title="Usuários" />
-        <div style={{ marginTop: "20px", width: "inherit" }}>
-          <NewUserForm />
-        </div>
-
-        <ListTile>
-          {data.map(({ userName }, index) => (
-            <Tile key={userName} count={index}>
-              {userName}
-            </Tile>
-          ))}
-        </ListTile>
-      </div>
-    </LoadingContainer>
+    <UserProvider>
+      <UsersComponent />
+    </UserProvider>
   );
 }
