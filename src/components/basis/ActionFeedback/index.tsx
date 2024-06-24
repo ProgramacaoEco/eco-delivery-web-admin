b929bf41a22eb45defb6d8810998abddb6aba918
Victor Proppe <vctudo.27@gmail.com>
2024-06-24 18:02:21 -0300

    feat: :sparkles: implement users management

---
+++ src/components/basis/ActionFeedback/index.tsx
@@ -0,0 +1,97 @@
+import {
+  IconButton,
+  Snackbar,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+
+import Close from "@icons/Close";
+import { useState } from "react";
+
+interface ActionFeedbackProps {
+  state: "success" | "error";
+  open: boolean;
+  message: string | undefined;
+  autoHideDuration?: number;
+}
+
+const Success = ({ message, open, autoHideDuration }: ActionFeedbackProps) => {
+  const [hide, setHide] = useState(open);
+
+  const theme = createTheme({
+    components: {
+      MuiSnackbarContent: {
+        styleOverrides: {
+          root: {
+            "&.MuiSnackbarContent-root": {
+              backgroundColor: "green",
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Snackbar
+        onClose={() => setHide(!hide)}
+        open={hide}
+        message={message}
+        autoHideDuration={autoHideDuration}
+      />
+    </ThemeProvider>
+  );
+};
+
+const Error = ({ message, open, autoHideDuration }: ActionFeedbackProps) => {
+  const [hide, setHide] = useState(open);
+
+  const theme = createTheme({
+    components: {
+      MuiSnackbarContent: {
+        styleOverrides: {
+          root: {
+            "&.MuiSnackbarContent-root": {
+              backgroundColor: "red",
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Snackbar
+        open={hide}
+        onClose={() => setHide(!hide)}
+        message={message}
+        autoHideDuration={autoHideDuration}
+      />
+    </ThemeProvider>
+  );
+};
+
+export default function ActionFeedback({
+  message,
+  open,
+  autoHideDuration,
+  state,
+}: ActionFeedbackProps) {
+  return state === "success" ? (
+    <Success
+      message={message}
+      open={open}
+      state={state}
+      autoHideDuration={autoHideDuration}
+    />
+  ) : (
+    <Error
+      message={message}
+      open={open}
+      state={state}
+      autoHideDuration={autoHideDuration}
+    />
+  );
+}
