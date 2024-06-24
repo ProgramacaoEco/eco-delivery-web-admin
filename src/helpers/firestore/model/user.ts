97f26f7196545d0dd393e21abd43890433ed620c
Victor Proppe <vctudo.27@gmail.com>
2024-06-24 18:02:38 -0300

    feat: :sparkles: implement users management

--- src/helpers/firestore/model/user.ts
+++ src/helpers/firestore/model/user.ts
@@ -1,13 +1,15 @@
 export class User implements BaseModel {
-  constructor(id: string, email: string, userName: string) {
+  constructor(id: string, email: string, userName: string, isAdmin?: boolean) {
     this._id = id;
     this._email = email;
     this._userName = userName;
+    this._isAdmin = isAdmin ?? false;
   }
 
   _id: string;
-  private _email: string;
-  private _userName: string;
+  private readonly _email: string;
+  private readonly _userName: string;
+  private readonly _isAdmin: boolean;
 
   get email() {
     return this._email;
@@ -15,5 +17,18 @@ export class User implements BaseModel {
 
   get userName() {
     return this._userName;
+  }
+
+  get isAdmin() {
+    return this._isAdmin;
+  }
+
+  toJson() {
+    return {
+      id: this._id,
+      email: this._email,
+      isAdmin: this._isAdmin,
+      name: this._userName,
+    };
   }
 }
