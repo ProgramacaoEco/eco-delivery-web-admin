fc426d3b9bf91660218e7913c88bb590ecae564f
Victor Proppe <vctudo.27@gmail.com>
2024-06-24 18:02:28 -0300

    feat: :sparkles: implement users management

--- src/components/basis/Tile/index.tsx
+++ src/components/basis/Tile/index.tsx
@@ -1,15 +1,24 @@
 import Delete from "@icons/Delete";
+import { IconButton } from "@mui/material";
 import { PropsWithChildren } from "react";
 import { themeVars } from "@/theme/theme.css";
 import { tile } from "./style.css";
 
 interface TileProps {
   count: number;
+  isDeletable?: boolean;
+  isEditable?: boolean;
+  onEdit?: () => void;
+  onDelete?: () => void;
 }
 
 export default function Tile({
   children,
   count,
+  isDeletable = true,
+  isEditable = false,
+  onEdit,
+  onDelete,
 }: PropsWithChildren<TileProps>) {
   const tileColor =
     count % 2 ? themeVars.color.tileEven : themeVars.color.tileOdd;
@@ -18,7 +27,15 @@ export default function Tile({
     <div>
       <div style={{ backgroundColor: tileColor }} className={tile}>
         {children}
-        <Delete />
+        {isEditable && <IconButton onClick={onEdit}></IconButton>}
+        {
+          <IconButton
+            style={{ visibility: isDeletable ? "visible" : "hidden" }}
+            onClick={onDelete}
+          >
+            <Delete />
+          </IconButton>
+        }
       </div>
     </div>
   );
