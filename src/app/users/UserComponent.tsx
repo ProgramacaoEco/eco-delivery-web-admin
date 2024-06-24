1052aa5aba9546aa5ed81b42467e1807218366d2
Victor Proppe <vctudo.27@gmail.com>
2024-06-24 18:25:07 -0300

    fix: :bug: fix show error rule

--- src/app/users/UserComponent.tsx
+++ src/app/users/UserComponent.tsx
@@ -1,5 +1,5 @@
 import { getSession, useSession } from "next-auth/react";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import ActionFeedback from "@/components/basis/ActionFeedback";
 import ListTile from "@/components/basis/ListTile";
@@ -7,6 +7,7 @@ import LoadingContainer from "@/components/basis/LoadingContainer";
 import NewUserForm from "./NewUserForm";
 import PageTitle from "@/components/basis/PageTitle/PageTitle";
 import Tile from "@/components/basis/Tile";
+import { User } from "@/helpers/firestore/model/user";
 import { UserContext } from "./context";
 import { themeVars } from "@/theme/theme.css";
 import useAddUser from "./useAddUser";
@@ -22,17 +23,31 @@ export default function UsersComponent() {
 
   const { loading, error, success, data, successMessage, errorMessage } =
     useContext(UserContext);
-
-  const [existingUser, setExistingUser] = useState(false);
-
   const { data: sessionData } = useSession();
 
-  const currentUser = data.filter(
-    ({ email }) => email === sessionData.user?.email
-  )[0];
+  const [existingUser, setExistingUser] = useState(false);
+  const [pageHasError, setPageHasError] = useState(false);
+
+  let currentUser: User;
+
+  if (!!data) {
+    currentUser = data.filter(
+      ({ email }) => email === sessionData?.user?.email
+    )[0];
+  }
+
+  useEffect(() => {
+    if (!loading) {
+      if (users.length <= 0) {
+        console.log(loading);
+        console.log(data.length);
+        setPageHasError(true);
+      }
+    }
+  }, [data, loading]);
 
   return (
-    <LoadingContainer loading={loading} error={typeof data === "undefined"}>
+    <LoadingContainer loading={loading} error={pageHasError}>
       <div className={users}>
         <PageTitle color={themeVars.color.users.appbarColor} title="Usuários" />
         <div style={{ marginTop: "20px", width: "inherit" }}>
