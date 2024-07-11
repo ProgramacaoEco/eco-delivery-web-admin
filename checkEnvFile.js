911d337daee465f6f4aab592e44661666ee0741e
Victor Proppe <vctudo.27@gmail.com>
2024-07-11 15:56:48 -0300

    chore: :lock: fix missing .env file interrupt builds

---
+++ checkEnvFile.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const requiredVariables = [
+  "NEXT_PUBLIC_API_KEY",
+  "NEXT_PUBLIC_AUTH_DOMAIN",
+  "NEXT_PUBLIC_DATABASE_URL",
+  "NEXT_PUBLIC_PROJECT_ID",
+  "NEXT_PUBLIC_STORAGE_BUCKET",
+  "NEXT_PUBLIC_MESSAGING_SENDER_ID",
+  "NEXT_PUBLIC_APP_ID",
+  "NEXT_PUBLIC_MEASUREMENT_ID",
+  "AUTH_SECRET",
+  "AUTH_GOOGLE_ID",
+  "AUTH_GOOGLE_SECRET",
+];
+
+requiredVariables.forEach((variable) => {
+  if (!process.env[variable]) {
+    console.error(
+      `Error: The ${variable} environment variable is empty or undefined`
+    );
+    process.exit(1); // Exit with an error code
+  } else {
+    console.info("All Variables are available");
+  }
+});
