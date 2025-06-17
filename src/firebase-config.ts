ab60b64b288df3658224d5c7e007650ef3e38c2e
robsonSampaio <robsoncs211@gmail.com>
2025-06-17 20:44:56 -0300

    ajustes

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -3,14 +3,10 @@ import {
   browserLocalPersistence,
   getAuth,
 } from "firebase/auth";
-// Import the functions you need from the SDKs you need
 import { getApps, initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -22,9 +18,19 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+// Inicialização do Firebase
 export const app =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+// 🔐 Inicialização do App Check com reCAPTCHA v3
+if (typeof window !== "undefined") {
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(
+      process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!
+    ),
+    isTokenAutoRefreshEnabled: true,
+  });
+}
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
