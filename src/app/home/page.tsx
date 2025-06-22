08e4d00d6ad2baa20bc1086b2014ad7f61fb8697
robsonSampaio <robsoncs211@gmail.com>
2025-06-22 20:31:49 -0300

    Adicionando cor aos card e background

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -112,7 +112,7 @@ export default function Page() {
               <Card
                 sx={{
                   flexGrow: 1,
-                  backgroundColor: "grey",
+                  backgroundColor: "#0e6ceaf7",
                   padding: 2,
                   textAlign: "center",
                 }}
--- src/theme/theme.css.ts
+++ src/theme/theme.css.ts
@@ -34,7 +34,7 @@ export const themeVars = createGlobalTheme(":root", {
     card: "#676767",
     tileEven: "#7A7A7A",
     tileOdd: "#545454",
-    background: "#274977",
+    background: "#333232",
     common: {
       white: "white",
       black: "black",
