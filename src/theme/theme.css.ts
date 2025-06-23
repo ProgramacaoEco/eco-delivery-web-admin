2205a8737a987748c894a81633bde6b26508fe07
robsonSampaio <robsoncs211@gmail.com>
2025-06-23 09:40:54 -0300

    Colocando o azul como cor principal

--- src/theme/theme.css.ts
+++ src/theme/theme.css.ts
@@ -34,7 +34,7 @@ export const themeVars = createGlobalTheme(":root", {
     card: "#676767",
     tileEven: "#7A7A7A",
     tileOdd: "#545454",
-    background: "#333232",
+    background: "#274977",
     common: {
       white: "white",
       black: "black",
